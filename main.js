1888d6d972fdce25c4b7f98db3b8775fa1e4f9ea
John Attai <donattai1032@gmail>
2022-07-10 02:01:26 +0100

    deleted login.html

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ const hamburger = document.querySelector(".hamburger");
 const navItems = document.querySelector(".nav-items");
 
 hamburger.addEventListener("click", function () {
-  // hamburger.classList.toggle('active')
   navItems.classList.toggle("active");
 });
